87087258dec617097df3dd18c87ba6dda3e74acc
Snap-sec <support@snapsec.co>
2025-11-02 16:06:26 +0530

    Adding Minlify Integration

---
+++ content/umami.js
@@ -0,0 +1,7 @@
+(function(){
+  var s = document.createElement('script');
+  s.defer = true;
+  s.src = 'https://cloud.umami.is/script.js';
+  s.setAttribute('data-website-id', '25788169-ab6d-43d6-b365-7b6e97a13a6b');
+  document.head.appendChild(s);
+})();
